d926dc7e07afac93f36bf16efd7c2685d410fdff
Ensley Ribeiro <ensley.ribeiro@ativa.solucoes>
2020-11-29 08:09:00 -0300

    test: tests basicos das controllers account e client para testar o CI    - Foi feito alguns testes das controllers Account e Client para      testar a integração do sistema com o CI.    - Foi removido o metodo da LuizalabsServerRestApplicationTests, pois      quero deixar somente a checagem das controllers.

--- luizalabs-server-rest/src/test/java/br/com/luizalabsserverrest/LuizalabsServerRestApplicationTests.java
+++ luizalabs-server-rest/src/test/java/br/com/luizalabsserverrest/LuizalabsServerRestApplicationTests.java
@@ -1,13 +1,9 @@
 package br.com.luizalabsserverrest;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LuizalabsServerRestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
